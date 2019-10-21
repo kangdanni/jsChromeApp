d49c893068e53387b26447a403faa4e7de00f614
강단 <kkangdanni@sk.com>
2019-10-21 16:40:06 +0900

    get location API

---
+++ weather.js
@@ -0,0 +1,38 @@
+const API_KEY = "3f8057ccfd140563cd6cca20618c804e";
+const COORDS = "coords";
+
+function saveCoords(coordsObj) {
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSuccess(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude,
+        longitude
+    };
+    saveCoords(coordsObj);
+}
+function handleGeoError() {
+    console.log("can't access");
+}
+
+
+function askForCoords(){
+    navigator.geolocation.getCurrentPosition(handleGeoSuccess,handleGeoError);
+}
+
+function loadCoords(){
+    const loadedCords = localStorage.getItem(COORDS);
+    if(loadedCords === null){
+        askForCoords();
+    }else {
+
+    }
+}
+
+function init(){
+    loadCoords()
+}
+init();
